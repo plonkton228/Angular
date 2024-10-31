ffff44dfded529830e2f1da6a589b725246e43ed
Plonkton228 <danik51002@gmail.com>
2024-10-31 23:59:22 +0300

    add classStyleJoiner

---
+++ src/app/share/lib/classJoin.ts
@@ -0,0 +1,16 @@
+﻿interface ClassesStyle {
+    mainClass: string,
+    otherClasses?: Array<String>,
+    booleanClasses?: Record<string, boolean>
+}
+
+
+export const classJoin = (args: ClassesStyle): string => {
+   return [
+    args.mainClass,
+    ...args.otherClasses || [],
+    ...Object.entries(args?.booleanClasses || []).filter(([key, value]) => value)
+    .map(([key,value]) => value)
+   ].join('')
+
+}
