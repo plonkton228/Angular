df21dbfd34b231f417706d6f9009a62a50d1ac00
Plonkton228 <danik51002@gmail.com>
2024-11-10 21:57:14 +0300

    Переход проекта на Nx

---
+++ generateConfigProjectJson.ts
@@ -0,0 +1,24 @@
+﻿const fs = require('fs');
+
+
+// Загрузка JSON-файлов
+const baseConfig = require('./projectConfigs/base.json');
+const build = require('./projectConfigs/build.json');
+const serve = require('./projectConfigs/serve.json');
+const additinal = require('./projectConfigs/additinal-options.json');
+const pre_build = require('./projectConfigs/pre-build.json');
+// Объединение конфигураций
+const mergedConfig = {
+  ...baseConfig,
+  targets: {
+    ...build,
+    ...serve,
+    ...additinal,
+    ...pre_build
+  }
+};
+
+
+fs.writeFileSync('project.json', JSON.stringify(mergedConfig, null, 2));
+
+
